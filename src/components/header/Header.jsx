5e720bbdc31fd4e046f5f18096cad8ab0b7b5b91
airlorm <elormnizi@gmail.com>
2024-06-12 10:07:20 +0300

    added different css scoping rules to header file and created new css file for the component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/logo.png";
+import classes from "./header.module.css";
+export default function Header() {
+  return (
+    <header>
+      <img src={logo} alt="A canvas" />
+      <h1>ReactArt</h1>
+      <p className={classes.bold}>A community of artists and art-lovers.</p>
+    </header>
+  );
+}
